82f4555fe68f1c300d0f8e432de7c433ecf2263a
gm-costa <gmaia_01@hotmail.com>
2023-04-18 00:30:44 -0300

    Aprimora utils

--- usuario/utils.py
+++ usuario/utils.py
@@ -1,40 +1,50 @@
 import re
+from random import randint, choice
 
-somente_numeros = "\d+"
-letras_minusculas = "[a-z]+"
-letras_maiusculas = "[A-Z]+"
 
-def verificar_forca_senha(texto):
-    def is_coincidente(texto, comparacao):
-        padrao = re.compile(comparacao)
-        retorno = re.fullmatch(padrao, texto)
-        return True if retorno else False
+def gera_senha():
+    string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*-+.!@#$'
+    tamanho = randint(2, 12)
+    senha = ''
+    for i in range(tamanho):
+        senha += choice(string)
+    return senha
 
-    if (len(texto) < 8) or (is_coincidente(texto, somente_numeros)) or (is_coincidente(texto, letras_minusculas)) or (is_coincidente(texto, letras_maiusculas)):
-        msg = 'senha fraca'
-    elif (is_coincidente(texto, '[a-z0-9]{8,20}')) or (is_coincidente(texto, '[A-Z0-9]{8,20}')):
-        msg = 'senha média'
-    elif (is_coincidente(texto, '[a-zA-Z0-9]{8,20}')) or (is_coincidente(texto, '[a-z0-9\W]{8,20}')):
-        msg = 'senha boa'
-    elif (is_coincidente(texto, '[\w\W]{8,20}')):
-        msg = 'senha forte'
-    else:
-        msg = f'senha deve ter no máximo 20 caracteres'
+def verificar_forca_senha(senha):
+    t_senha = len(senha)
 
-    # print(f'{msg}, foi passado {len(texto)} caracteres')
-    return f'({msg})'
+    tem_numero = True if re.search(re.compile('\d'), senha) else False 
+    tem_minuscula = True if re.search(re.compile('[a-z]'), senha) else False
+    tem_maiuscula = True if re.search(re.compile('[A-Z]'), senha) else False
+    tem_char_especial = True if re.search(re.compile('\W'), senha) else False
+
+    so_numeros = re.fullmatch(re.compile('\d+'), senha)
+    so_minusculas = True if re.fullmatch(re.compile('[a-z]+'), senha) else False
+    so_maiusculas = True if re.fullmatch(re.compile('[A-Z]+'), senha) else False
+
+    letras_numeros = (tem_minuscula and tem_numero) or (tem_maiuscula and tem_numero)
+    min_mai_numeros = tem_minuscula and tem_maiuscula and tem_numero
+    min_num_especial = tem_minuscula and tem_numero and tem_char_especial
+    mai_num_especial = tem_maiuscula and tem_numero and tem_char_especial
+    complexa = tem_minuscula and tem_maiuscula and tem_numero and tem_char_especial
+
+    s_fraca = (t_senha < 8) or (so_minusculas or so_maiusculas or so_numeros)
+    s_media = (t_senha >= 8) and letras_numeros
+    s_boa = (t_senha >= 8) and (min_mai_numeros or min_num_especial or mai_num_especial)
+    s_forte = (t_senha >= 8) and complexa
+
+    if s_forte:
+        msg = 'forte'
+    elif s_boa:
+        msg = 'boa'
+    elif s_media:
+        msg = 'média'
+    elif s_fraca:
+        msg = 'fraca'
+
+    return f"'{senha}' é {msg}  ==> tamanho: {t_senha}"
 
 if __name__ == '__main__':
 
-    # verificar_forca_senha("fulano")
-    # verificar_forca_senha("f123455")
-    # verificar_forca_senha("fula12no")
-    # verificar_forca_senha("fula12nof34")
-    # verificar_forca_senha("fula12nofHgfda12Yz")
-    # verificar_forca_senha("fula12nof/Hgfda12")
-    # verificar_forca_senha("fula12nof*gfda12&a45")
-    # verificar_forca_senha("fula12nof*gf7da12&a45")
-    # verificar_forca_senha("fula12nof*gfda12&a45")
-    # verificar_forca_senha("fula12noH*gfda12&a45")
-
-    print(verificar_forca_senha("fula12noH*gfda12&a45"))
+    for i in range(5):
+        print(verificar_forca_senha(gera_senha()))
